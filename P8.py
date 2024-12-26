f84488918816b133768cd9c0fd0f469c452cb33c
Space <143919787+Rhcs2023@users.noreply.github.com>
2024-12-26 11:30:52 -0600

    Update P8.py

--- P8.py
+++ P8.py
@@ -4,13 +4,13 @@ import tempfile
 
 # Diccionario con palabras a traducir
 diccionario = {
-    'a': '1 ',
-    'b': '2 ',
-    'c': '3 ',
-    'd': '4 ',
-    'e': '5 ',
-    'f': '6 ',
-    'g': '7 ',
+    'a': 'uno ',
+    'b': 'dos ',
+    'c': 'tres',
+    'd': 'cuatro ',
+    'e': 'cinco ',
+    'f': 'seis ',
+    'g': 'siete ',
     'mi': 'ri ',
 
 }
